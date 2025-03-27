01cb7050205ba80c1001128e953213f52d79f28d
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2025-03-27 12:38:17 +0800

    Add files via upload

---
+++ EVA2_CUENTA_BANCO_11/src/main/java/com/mycompany/eva2_cuenta_banco_11/EVA2_CUENTA_BANCO_11.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_cuenta_banco_11;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA2_CUENTA_BANCO_11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
