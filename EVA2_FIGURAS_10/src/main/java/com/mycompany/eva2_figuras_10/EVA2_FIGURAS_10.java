c2704a5d6acb8bc24fd84c519e96ab59c69c9e04
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2025-03-27 12:12:59 +0800

    Add files via upload

---
+++ EVA2_FIGURAS_10/src/main/java/com/mycompany/eva2_figuras_10/EVA2_FIGURAS_10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_figuras_10;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA2_FIGURAS_10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
