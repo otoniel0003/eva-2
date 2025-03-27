cc7c191c0ea8dcb56ad996fd83b8090276a614ec
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2025-03-27 11:40:03 +0800

    Add files via upload

---
+++ EVA2_CLASES_ABSTRACTAS_9/src/main/java/com/mycompany/eva2_clases_abstractas_9/EVA2_CLASES_ABSTRACTAS_9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_clases_abstractas_9;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA2_CLASES_ABSTRACTAS_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
