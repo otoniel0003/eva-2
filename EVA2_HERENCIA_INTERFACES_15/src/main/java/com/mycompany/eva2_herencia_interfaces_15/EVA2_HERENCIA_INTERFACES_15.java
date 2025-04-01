fa933bda82e66c62e9b7b8589dc9f23413ea256f
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2025-04-01 11:00:03 +0800

    Add files via upload

---
+++ EVA2_HERENCIA_INTERFACES_15/src/main/java/com/mycompany/eva2_herencia_interfaces_15/EVA2_HERENCIA_INTERFACES_15.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_herencia_interfaces_15;
+
+/**
+ *
+ * @author Salid
+ */
+public class EVA2_HERENCIA_INTERFACES_15 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
